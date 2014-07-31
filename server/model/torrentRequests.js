56dfa7ec55a36f81991f97613100df6538eb4b43
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2014-07-31 04:29:43 +0200

    Node dependency and basic UI

---
+++ server/model/torrentRequests.js
@@ -0,0 +1,13 @@
+﻿var mongoose = require('mongoose');
+
+exports.torrentRequestList = function torrentRequestList(callback) {
+	var Requests = mongoose.model('TorrentRequest');
+	Requests.find(function (err, teams) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(teams);
+			callback("", teams);
+		}
+	}); // end Team.find
+}; // end exports.teamlist
